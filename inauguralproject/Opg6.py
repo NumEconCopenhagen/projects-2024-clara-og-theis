606fe4e5dfa0f7f8246718faef5e820cebf95846
Theis <theisscheuerjansen@gmail.com>
2024-03-21 17:06:28 +0100

    opg6

---
+++ inauguralproject/Opg6.py
@@ -0,0 +1,10 @@
+import numpy as np
+from types import SimpleNamespace 
+from scipy import optimize
+from matplotlib import pyplot as plt
+from ExchangeEconomy import ExchangeEconomyClass
+
+#a
+
+# Initialize the economy
+economy = ExchangeEconomyClass()
