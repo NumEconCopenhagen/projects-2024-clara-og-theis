00a94b6a7b3b47550e7f85dc74107645955de525
Theis <theisscheuerjansen@gmail.com>
2024-03-24 15:51:02 +0100

    test

--- inauguralproject/ExchangeEconomy.py
+++ inauguralproject/ExchangeEconomy.py
@@ -233,11 +233,7 @@ class ExchangeEconomyClass:
         # a. objective function (to minimize) 
         obj = lambda x: -self.utility_A(x[0], x[1])-self.utility_B(1-x[0], 1-x[1])  # utility function
 
-<<<<<<< HEAD
         # b. Bounds
-=======
-        # b. bounds
->>>>>>> 744bc7bc5143599e5eaec358994cce633cb7c01c
         bounds = ((0,1),(0,1))
 
         # c. call solver, use SLSQP
