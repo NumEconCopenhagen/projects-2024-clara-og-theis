9213ed94ed8a8212a38e3c0b31cbedef87a61fc7
fqc147 <fqc147@alumni.ku.dk>
2024-05-05 17:44:25 +0200

    start

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,6 +1,8 @@
 from scipy import optimize
 from types import SimpleNamespace
 import sympy as sm
+import numpy as np
+import matplotlib.pyplot as plt
 
 class NashBargainingClass:
     
@@ -9,6 +11,7 @@ class NashBargainingClass:
 
         par = self.par = SimpleNamespace()
         val = self.val = SimpleNamespace()
+        sim = self.sim = SimpleNamespace()
 
         # model parameters for analytical solution
         par.w = sm.symbols('w')
@@ -22,9 +25,17 @@ class NashBargainingClass:
         val.d1 = 10
         val.d2 = 0
         val.alpha = 1/3
+        val.alpha_vec = np.linspace(0,1,10)
+
+        # parameter values for simulation
+        sim.N = 10000
+        sim.theta = np.random.normal(60, 10, sim.N)
+        sim.d1 = 10
+        sim.d2 = 0
+        sim.alpha = 1/3
 
     def analyticalsolution(self):
-        " solve the one shot Nash bargaining problem analytically "
+        " solve the Nash bargaining problem analytically "
     
         par = self.par
 
@@ -56,7 +67,7 @@ class NashBargainingClass:
         val = self.val
 
         # a. Objective function
-        obj = lambda w: -((self.utility_1(w)**val.alpha)*(self.utility_2(w)**(1-val.alpha)))  
+        obj = lambda w: -(((self.utility_1(w)-val.d1)**val.alpha)*((self.utility_2(w)-val.d2)**(1-val.alpha)))
 
         # b. initial guess and bounds
         bounds = [(val.d1, val.theta)]
@@ -64,6 +75,67 @@ class NashBargainingClass:
 
         #. b. Maximize surplus
         result = optimize.minimize(obj, initial_guess, bounds=bounds, method='Nelder-Mead') 
-        #OBS overvej optimizer, fejl
+
+        w =  f'w = {result.x[0]:.3f}'
     
-        return result
+        return w
+    
+    def varyingalpha(self):
+        " solve the one shot Nash bargaining problem numerically for varying alpha values"
+    
+        val = self.val
+        w_values = []  # Store w values for each alpha
+
+        for alpha in val.alpha_vec:
+            # a. Objective function
+            obj = lambda w: -(((self.utility_1(w)-val.d1)**alpha)*((self.utility_2(w)-val.d2)**(1-alpha)))
+
+            # b. initial guess and bounds
+            bounds = [(val.d1, val.theta)]
+            initial_guess = val.d1
+
+            #. b. Maximize surplus
+            result = optimize.minimize(obj, initial_guess, bounds=bounds, method='Nelder-Mead') 
+
+            w = result.x[0]
+            w_values.append(w)
+
+        # Create a new figure
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+
+        ax.plot(val.alpha_vec, w_values)
+        ax.set_xlabel('Alpha')
+        ax.set_ylabel('Wage')
+        ax.set_title('Wage as a function of alpha')
+        ax.grid(True)
+    
+        return fig
+    
+    def simulatewdistribution(self):
+        sim = self.sim
+
+        w_values = []  # Store w values for each individual
+        
+        for theta in sim.theta:
+            # a. Objective function
+            obj = lambda w: -(((self.utility_1(w)-sim.d1)**sim.alpha)*((theta-w-sim.d2)**(1-sim.alpha)))
+            
+            # b. initial guess and bounds
+            bounds = [(sim.d1, theta)]
+            initial_guess = sim.d1
+
+            # Maximize surplus
+            result = optimize.minimize(obj, initial_guess, bounds=bounds, method='Nelder-Mead') 
+
+            w = result.x[0]
+            w_values.append(w)
+
+        # Plot the distribution of w
+        plt.hist(w_values, bins=100)  # Adjust the number of bins as needed
+        plt.xlabel('Wage')
+        plt.ylabel('Frequency')
+        plt.title('Distribution of Wages')
+        plt.grid(True)
+        plt.show()
+    
