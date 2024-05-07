e499f32a6bcefb4834280790091c77a5eb16dc07
fqc147 <fqc147@alumni.ku.dk>
2024-05-07 10:51:14 +0200

    bud

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -28,20 +28,27 @@ class NashBargainingClass:
         val.alpha_vec = np.linspace(0,1,10)
 
         # parameter values for simulation
+        np.random.seed(100)  
         sim.N = 10000
         sim.theta = np.random.normal(60, 10, sim.N)
         sim.d1 = 10
         sim.d2 = 0
         sim.alpha = 1/3
+        sim.m  = 20
 
     def analyticalsolution(self):
-        " solve the Nash bargaining problem analytically "
+        """ solve the Nash bargaining problem analytically """
     
         par = self.par
 
+        # a. define utility 
         u1 = par.w
         u2 = par.theta - par.w
+
+        # b. define the object
         objective = ((u1-par.d1)**par.alpha)*(u2)**(1-par.alpha)
+
+        # c. find the solution as the wage where the differentiated objective equals zero (FOC)
         foc = sm.diff(objective,par.w)
         sol = sm.solve(foc,par.w)[0]
 
@@ -62,39 +69,39 @@ class NashBargainingClass:
         return val.theta - w
     
     def numericalsolution(self):
-        " solve the one shot Nash bargaining problem numerically"
+        """ solve the Nash bargaining problem numerically"""
     
         val = self.val
 
-        # a. Objective function
+        # a. objective function
         obj = lambda w: -(((self.utility_1(w)-val.d1)**val.alpha)*((self.utility_2(w)-val.d2)**(1-val.alpha)))
 
         # b. initial guess and bounds
         bounds = [(val.d1, val.theta)]
         initial_guess = val.d1
 
-        #. b. Maximize surplus
+        # c. maximize surplus
         result = optimize.minimize(obj, initial_guess, bounds=bounds, method='Nelder-Mead') 
 
-        w =  f'w = {result.x[0]:.3f}'
+        w = f'w = {result.x[0]:.1f}'
     
         return w
     
     def varyingalpha(self):
-        " solve the one shot Nash bargaining problem numerically for varying alpha values"
+        """ solve the Nash bargaining problem numerically for varying alpha values """
     
         val = self.val
         w_values = []  # Store w values for each alpha
 
         for alpha in val.alpha_vec:
-            # a. Objective function
+            # a. objective function
             obj = lambda w: -(((self.utility_1(w)-val.d1)**alpha)*((self.utility_2(w)-val.d2)**(1-alpha)))
 
             # b. initial guess and bounds
             bounds = [(val.d1, val.theta)]
             initial_guess = val.d1
 
-            #. b. Maximize surplus
+            # c. maximize surplus
             result = optimize.minimize(obj, initial_guess, bounds=bounds, method='Nelder-Mead') 
 
             w = result.x[0]
@@ -109,8 +116,7 @@ class NashBargainingClass:
         ax.set_ylabel('Wage')
         ax.set_title('Wage as a function of alpha')
         ax.grid(True)
-    
-        return fig
+        plt.show()
     
     def simulatewdistribution(self):
         sim = self.sim
@@ -125,17 +131,48 @@ class NashBargainingClass:
             bounds = [(sim.d1, theta)]
             initial_guess = sim.d1
 
-            # Maximize surplus
+            # c. maximize surplus
             result = optimize.minimize(obj, initial_guess, bounds=bounds, method='Nelder-Mead') 
 
             w = result.x[0]
             w_values.append(w)
 
         # Plot the distribution of w
-        plt.hist(w_values, bins=100)  # Adjust the number of bins as needed
+        plt.hist(w_values, bins=100, range=(15, 40))  
         plt.xlabel('Wage')
         plt.ylabel('Frequency')
         plt.title('Distribution of Wages')
-        plt.grid(True)
+        plt.grid(alpha=0.3)
         plt.show()
+
+    def minimumwage(self):
+        sim = self.sim
+
+        w_values = []  # Store w values for each individual
+        
+        for theta in sim.theta:
+            if theta >= sim.m:
+                # a. Objective function
+                obj = lambda w: -(((self.utility_1(w)-sim.d1)**sim.alpha)*((theta-w-sim.d2)**(1-sim.alpha)))
+            
+                # b. initial guess and bounds
+                bounds = [(sim.m, theta)]
+                initial_guess = sim.m
+
+                # c. maximize surplus
+                result = optimize.minimize(obj, initial_guess, bounds=bounds, method='Nelder-Mead') 
+
+                w = result.x[0]
+                w_values.append(w)
+
+        # Plot the distribution of w
+        plt.hist(w_values, bins=100, range=(15, 40)) 
+        plt.xlabel('Wage')
+        plt.ylabel('Frequency')
+        plt.title('Distribution of Wages')
+        plt.grid(alpha=0.3)
+        plt.show()
+    
+    
+    
     
