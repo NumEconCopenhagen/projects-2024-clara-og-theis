67078ff6a0e08e8c4067e1ae17e04151dcd9d478
fqc147 <fqc147@alumni.ku.dk>
2024-05-15 13:50:32 +0200

    Rettelser

--- inauguralproject/ExchangeEconomy.py
+++ inauguralproject/ExchangeEconomy.py
@@ -59,7 +59,7 @@ class ExchangeEconomyClass:
         X2B = (1-par.beta)*IB
         return X1B,X2B
 
-    def find_pareto_improvements(self,N1,N2,do_print=True):
+    def find_pareto_improvements(self,N1,N2):
         """ find pareto improvements compared to initial endowment """
 
         par = self.par
@@ -125,7 +125,7 @@ class ExchangeEconomyClass:
 
         return eps1
     
-    def find_equilibrium(self, p_low, p_high, do_grid_search = True,do_print = True):
+    def find_equilibrium(self, do_grid_search = True,do_print = True):
         """ find market clearing price """
         
         par = self.par
@@ -148,81 +148,79 @@ class ExchangeEconomyClass:
                     p_high = p1_values_array[i]
                     found_bracket = True
         
-        print(f'\nEquilibrium is in interval [{p_low:.2f}, {p_high:.2f}]')
-
         # c. find the equilibrium price
         p1_eq = optimize.brentq(self.excess, p_low, p_high)
+
         if do_print:
-            print(f'\nMarket clearing price: {p1_eq:.2f}')
+            print(f'\nMarket clearing price: {p1_eq:.4f}')
 
         # d. check that the markets clear
         eps1, eps2 = self.check_market_clearing(p1_eq)
 
         if do_print:
             print(f'\nMarket clearing errors: eps1 = {eps1:.2f}, eps2 = {eps2:.2f}')
+            return None
 
+        return p1_eq
+        
 
     def Utility_max_a(self,do_print=True):
         """ maximize utility of consumer A for prices P"""
 
-        # Define initial conditions for best utility and corresponding price
+        # a. Define initial conditions for best utility and corresponding price
         utility_best = -np.inf
         best_p1 = np.nan
         p1_values = list(0.5 + 2 * (i / 75) for i in range(76))
         p1_values_array = np.array(p1_values)
 
-        # Iterate over each value of p1
+        # b. Iterate over each value of p1
         for p1 in p1_values_array:
-            # a. calculate A's demand and B's demand for goods 1 and 2 at price p1
+            # i. calculate A's demand and B's demand for goods 1 and 2 at price p1
             x1A, x2A = self.demand_A(p1)  # Use agent A's demand
             x1B, x2B = self.demand_B(p1)  # Use agent B's demand
 
-        # b. Calculate the utility for agent A using the given formula
+            # ii. Calculate the utility for agent A using the given formula
             utility = self.utility_A(1-x1B, 1-x2B)
 
-        # c. Update the best utility and corresponding p1 if the current utility is higher
+            # iii. Update the best utility and corresponding p1 if the current utility is higher
             if utility > utility_best:
                 utility_best = utility
                 best_p1 = p1
-                best_X1A = x1A
-                best_X2A = x2A
+                best_X1A = 1-x1B
+                best_X2A = 1-x2B
         
         if do_print: 
-            print(f'Best utility is {utility_best:.8f} at p1 = {best_p1:.8f}')
-            print(f'Optimal allocation for A is as follows, x1: {best_X1A:.8f} and x2: {best_X2A:.8f}')
+            print(f'Consumer As maximised utility is {utility_best:.4f} at p1 = {best_p1:.4f}')
+            print(f'The allocation is then X1A = {best_X1A:.4f} and X2A = {best_X2A:.4f}')
+            return None
 
-        return best_X1A, best_X2A
-
+        return best_X1A, best_X2A 
+            
     def Utility_max_b(self,do_print=True):
-        """ maximize utility of consumer A for prices P"""
-
         # Define initial conditions for best utility and corresponding price
-        utility_best = -np.inf
-        best_p1 = np.nan
-        p1_values_array = np.linspace(0.0001,100,1000)
 
-        # Iterate over each value of p1
-        for p1 in p1_values_array:
-            # a. calculate A's demand and B's demand for goods 1 and 2 at price p1
-            x1A, x2A = self.demand_A(p1)  # Use agent A's demand
-            x1B, x2B = self.demand_B(p1)  # Use agent B's demand
+        par = self.par
 
-        # b. Calculate the utility for agent A using the given formula
-            utility = self.utility_A(1-x1B, 1-x2B)
+        # a. objective function (to minimize) 
+        obj = lambda x: -self.utility_A(1-self.demand_B(x[0])[0], 1-self.demand_B(x[0])[1])  # utility function
 
-        # c. Update the best utility and corresponding p1 if the current utility is higher
-            if utility > utility_best:
-                utility_best = utility
-                best_p1 = p1
-                best_X1A = x1A
-                best_X2A = x2A
-        
+        # b. call solver, use SLSQP
+        initial_guess = 1 
+
+        res = optimize.minimize(obj, initial_guess, method='SLSQP')
+    
+        # c. unpack and print solution
+        p1 = res.x[0]
+        X1A = 1 - self.demand_B(p1)[0]
+        X2A = 1 - self.demand_B(p1)[1]
+        uA = self.utility_A(X1A, X2A)
+
         if do_print: 
-            print(f'Best utility is {utility_best:.8f} at p1 = {best_p1:.8f}')
-            print(f'Optimal allocation for A is as follows, x1: {best_X1A:.8f} and x2: {best_X2A:.8f}')
-                    
-        return best_X1A, best_X2A
-
+            print(f'Consumer As maximised utility is {uA:.4f} at p1 = {p1:.4f}')
+            print(f'The allocation is then X1A = {X1A:.4f} and X2A = {X2A:.4f}')
+            return None
+        
+        return X1A, X2A
 
     def pareto_optimizer(self,do_print=True):
         """ maximize utility of consumer A in pareto improvements"""
@@ -244,14 +242,13 @@ class ExchangeEconomyClass:
             if uA > uA_best:
                 X1A_best = X1A
                 X2A_best = X2A 
-                u_best = uA
+                uA_best = uA
 
-        # c. print solution
-        if do_print:
-            self.print_solution(X1A_best,X2A_best,u_best) 
-    
-        return X1A_best, X2A_best, u_best
+        if do_print: 
+            print(f'The allocation is X1A = {X1A_best:.4f} and X2A = {X2A_best:.4f} with utility of consumer A at {uA_best:.4f}')
+            return None
 
+        return X1A_best, X2A_best
 
     def marketmaker_solver(self,do_print=True):
         """ maximize utility of consumer A where A is market maker"""
@@ -275,17 +272,12 @@ class ExchangeEconomyClass:
         x2A = res.x[1]
         uA = self.utility_A(x1A, x2A)
 
-        if do_print:
-            self.print_solution(x1A,x2A,uA)   
+        if do_print: 
+            print(f'The allocation is X1A = {x1A:.4f} and X2A = {x2A:.4f} with utility of consumer A at {uA:.4f}')
+            return None
 
-        return x1A, x2A, uA
+        return x1A, x2A 
 
-    def print_solution(self,x1A,x2A,uA):
-        """ print solution """
-
-        print(f'x1A = {x1A:.4f}')
-        print(f'x2A = {x2A:.4f}')
-        print(f'uA  = {uA:.4f}')
    
     def socialplanner_solver(self,do_print=True):
         """ maximize aggregate utility """
@@ -306,93 +298,52 @@ class ExchangeEconomyClass:
         # d. unpack and print solution
         x1A = res.x[0]
         x2A = res.x[1]
+        x1B = 1 - x1A
+        x2B = 1 - x2A
         uA = self.utility_A(x1A, x2A)
+        uB = self.utility_B(x1B, x2B)
 
-        if do_print:
-            self.print_solution(x1A,x2A,uA)   
+        if do_print: 
+            print(f'The allocation is X1A = {x1A:.4f}, X2A = {x2A:.4f}, X1B = {x1B:.4f} and X2B = {x2B:.4f}')
+            print(f'The utility of consumer A is {uA:.4f} and the utility of consumer B is {uB:.4f}')
+            return None
 
-        return x1A, x2A, uA
+        return x1A, x2A
 
-    def setw(self, s1,s2):
+    def setw(self, s):
         """ create random set of endowments """
 
         # a. set seed
         np.random.seed(2000)
 
         # b. draw uniformly distributed endowments
-        w1A = np.random.uniform(size=s1)
-        w2A = np.random.uniform(size=s2)
+        w1A = np.random.uniform(size=s)
+        w2A = np.random.uniform(size=s)
 
         # c. create a random set of endowments
-        W  = list(zip(w1A,w2A))
+        W = []
+        for i in range(s):
+            W.append((w1A[i], w2A[i]))
         
         return W
-
-
-    def demand_A_wset(self,p1, w1A, w2A):
-        """ calculate demand of consumer A with variable endowment"""
-
-        par = self.par
-
-        #a. Income
-        IA = p1*w1A+w2A
-
-        #b. Demand for good 1
-        X1A = par.alpha*(IA/p1)
-
-        #c. Demand for good 2
-        X2A = (1-par.alpha)*IA
-        return X1A,X2A
-
-    def demand_B_wset(self,p1, w1A, w2A):
-        """ calculate demand of consumer B with variable endowment"""
-
-        par = self.par
-
-        #a. Income 
-        IB = p1*(1-w1A)+(1-w2A)
-
-        #b. Demand for good 1
-        X1B = par.beta*(IB/p1)
-
-        #c. Demand for good 2
-        X2B = (1-par.beta)*IB
-        return X1B,X2B
-
-    def excess_wset(self, p1, w1A, w2A):
-        """ calculate excess demand for good 1 """
-        par = self.par
-
-        # a. define demand for each endowment set
-        x1A,x2A = self.demand_A_wset(p1, w1A, w2A)
-        x1B,x2B = self.demand_B_wset(p1, w1A, w2A)
-        
-        # c. calculate market error for the market of good 1 for each endowment combination
-        eps1 = x1A - w1A + x1B - (1 - w1A)
-
-        return eps1
     
-    def find_equilibrium_wset(self, w1A, w2A, do_grid_search = True):
-        """ find price interval for each endowment combination """
-        
+    def equilibriumallocation(self):
+        """ find the market equilibrium allocation """
+
         par = self.par
 
-        # a. define prices
-        p1_values = np.linspace(0.01, 100, 10000) 
+        allocation=[]
 
-        # b. find price interval where the market error is close to zero
-        if do_grid_search:
-            found_bracket = False
-            for i,p in enumerate(p1_values):
-                excess = self.excess_wset(p, w1A, w2A)
+        for i in range(50):
+            w1A, w2A = self.setw(s=50)[i]
+            par.w1A=w1A
+            par.w2A=w2A
 
-                # save the bracket that contains 0
-                if excess < 0 and not found_bracket:
-                    p_low = p1_values[i-1]
-                    p_high = p1_values[i]
-                    found_bracket = True
+            p1_eq = self.find_equilibrium(do_print=False)
+            x1A, x2A = self.demand_A(p1_eq)
 
-        # c. find the equilibrium price
-        p1_eq = optimize.brentq(self.excess_wset, p_low, p_high, args=(w1A, w2A))
-        x1A,x2A = self.demand_A_wset(p1_eq, w1A, w2A)
-        return x1A,x2A
+            allocation.append((x1A, x2A))
+
+        return allocation
+
+
